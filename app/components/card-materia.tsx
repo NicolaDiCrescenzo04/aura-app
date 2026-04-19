527d9e398098aab001742dc08ae9d795713b086d
Nicola <nicoladicrescenzo04@icloud.com>
2026-04-19 13:15:57 +0200

    Card schema e fetch con dati statici

--- app/components/card-materia.tsx
+++ app/components/card-materia.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
 
-type MateriaProps = {
-  nome: string
-  numero_schemi: number
+type CardMateriaProps = {
+  id: string;
+  nome: string;
+  numero_schemi: number;
 }
 
-const CardMateria = ({ nome, numero_schemi }: MateriaProps) => {
+const CardMateria = ({ id, nome, numero_schemi }: CardMateriaProps ) => {
   return (
-  <Link href={`/materie/${nome.toLowerCase()}`} className="rounded-lg p-4 m-4 w-32 h-32 flex flex-col items-center justify-center">
+  <Link href={`/materie/${id}`} className="rounded-lg p-4 m-4 w-32 h-32 flex flex-col items-center justify-center">
 
       <p className="uppercase font-bold">{nome}</p>
       <p className="text-xs">{numero_schemi} schemi</p>
